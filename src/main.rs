5e317db6a66d2acd555b3e9decbee2f335716739
Matej Koncal <matej.koncal@resco.net>
2022-09-07 08:41:18 +0200

    change qr code decoder

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 use arboard::Clipboard;
 use image::{DynamicImage, ImageBuffer};
-use string_builder::Builder;
 
 fn main() {
     let mut clipboard = Clipboard::new().unwrap();
@@ -12,8 +11,6 @@ fn main() {
         }
     };
 
-    let decoder = bardecoder::default_decoder();
-
     let image_buffer = ImageBuffer::from_raw(
         img.width.try_into().unwrap(),
         img.height.try_into().unwrap(),
@@ -22,49 +19,60 @@ fn main() {
     .unwrap();
 
     let dyn_image = DynamicImage::ImageRgba8(image_buffer);
-    let qr_decoder_result = decoder.decode(&dyn_image);
+    let grey_image = dyn_image.to_luma8();
+    let mut decoder = quircs::Quirc::default();
+    let mut codes = decoder.identify(
+        grey_image.width() as usize,
+        grey_image.height() as usize,
+        &grey_image,
+    );
 
-    let mut str_builder = Builder::default();
-    for line in qr_decoder_result.first().unwrap().as_ref().unwrap().lines() {
+    let code = codes.next().unwrap().unwrap();
+    let content = code.decode().unwrap();
+    let str_content = std::str::from_utf8(&content.payload).unwrap();
+
+    let mut result = String::new();
+
+    for line in str_content.lines() {
         let line_vec: Vec<&str> = line.split("=").collect();
         let key = line_vec.first().unwrap().to_owned();
 
         match key {
             "Url" => {
-                str_builder.append("export");
-                str_builder.append(" ");
-                str_builder.append("WebServiceURL");
-                str_builder.append("=");
-                str_builder.append(line_vec.last().unwrap().to_owned());
-                str_builder.append(";");
+                result.push_str("export");
+                result.push_str(" ");
+                result.push_str("WebServiceURL");
+                result.push_str("=");
+                result.push_str(line_vec.last().unwrap().to_owned());
+                result.push_str(";");
 
-                str_builder.append("export");
-                str_builder.append(" ");
-                str_builder.append("BackendURL");
-                str_builder.append("=");
-                str_builder.append(line_vec.last().unwrap().to_owned());
-                str_builder.append(";");
+                result.push_str("export");
+                result.push_str(" ");
+                result.push_str("BackendURL");
+                result.push_str("=");
+                result.push_str(line_vec.last().unwrap().to_owned());
+                result.push_str(";");
             }
             "UserName" => {
-                str_builder.append("export");
-                str_builder.append(" ");
-                str_builder.append("DebugLogin");
-                str_builder.append("=");
-                str_builder.append(line_vec.last().unwrap().to_owned());
-                str_builder.append(";");
+                result.push_str("export");
+                result.push_str(" ");
+                result.push_str("DebugLogin");
+                result.push_str("=");
+                result.push_str(line_vec.last().unwrap().to_owned());
+                result.push_str(";");
             }
             "Password" => {
-                str_builder.append("export");
-                str_builder.append(" ");
-                str_builder.append("DebugPwd");
-                str_builder.append("=");
-                str_builder.append("\"");
-                str_builder.append(line_vec.last().unwrap().to_owned());
-                str_builder.append("\"");
-                str_builder.append(";");
+                result.push_str("export");
+                result.push_str(" ");
+                result.push_str("DebugPwd");
+                result.push_str("=");
+                result.push_str("\"");
+                result.push_str(line_vec.last().unwrap().to_owned());
+                result.push_str("\"");
+                result.push_str(";");
             }
             _ => {}
         }
     }
-    println!("{}", str_builder.string().unwrap());
+    println!("{}", result);
 }
